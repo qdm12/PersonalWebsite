b572a68ae0b4ebbe781917925697f03462bfde24
quentin mcgaw <quentin.mcgaw@gmail.com>
2017-01-20 18:29:40 -0500

    Added Download resume animations

--- javascript/functions.js
+++ javascript/functions.js
@@ -23,8 +23,7 @@ function skype_button() {
 }
 function interactive_gradient_background() {
     $("#Home").mousemove(function( event ) {
-        var w = $(this).width(),
-            pct = 360*(+event.pageX)/w/1.6,
+        var pct = 90*(+event.pageX)/$(this).width() + 90*(+event.pageY)/$(this).height(),
             bg = "linear-gradient(" + pct + "deg,#2f8770, #355d9f, #ff884d)";
         $("#Home").css("background-image", bg);
     });
@@ -91,11 +90,13 @@ function center_download_resume() {
     center_download.mouseover(function(){
 		center_download.stop().animate({
 			"opacity":"1.0",
+            "font-size":"75%",
 		}, 800);
     });
     center_download.mouseout(function(){
 		center_download.stop().animate({
 			"opacity":"0.5",
+            "font-size":"70%",
 		}, 800);
     });
 }
