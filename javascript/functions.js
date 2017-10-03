ec9a77fa445ac64d3a4b659f493d650ef8cb408c
Quentin McGaw <quentin.mcgaw@gmail.com>
2017-10-03 01:43:01 -0400

    Updated Tagcanvas; added skills page; finalized timeline and map

--- javascript/functions.js
+++ javascript/functions.js
@@ -8,7 +8,46 @@ function changeOpacity(id, opacity) {
 	document.getElementById(id).style.opacity = opacity;
 	return false;
 }
-
+function moreDetailsOnSkill(skillboxID) {
+	var display = document.getElementById(skillboxID);
+	display.innerHTML = "";
+	switch (skillboxID){
+		case "Crypto":
+			display.innerHTML = "Bitcoin<br>Cryptography<br>Ethereum<br>Homomorphic encryption thesis<br>Network and web security<br>Kali Linux";
+			break;
+		case "Programming":
+			display.innerHTML = "Python 2 and 3<br>C++ and C<br>Java and Android<br>Javascript<br>Scala<br>RabbitMQ";
+			break;
+		case "Devops":
+			display.innerHTML = "Github, Zenhub, GitKraken & BitBucket<br>Vagrant and Virtual Box<br>Docker and IBM Bluemix<br>Travis CI and unit testing<br>Swagger and RESTful APIs";
+			break;
+		case "Web":
+			display.innerHTML = "HTML, CSS, Javascript, a bit of PHP<br>Database Systems: SQL and Redis<br>Network architecture course";
+			break;
+		case "Design":
+			display.innerHTML = "Adobe XD<br>inVision<br>Gimp<br>Patience and a decent taste";
+			break;
+		case "HPC":
+			display.innerHTML = "Hadoop (in Java and Python)<br>GPUs with openCL (C++)<br>Multi-core / Multi-threading<br>Cloudera QuickStart VM";
+			break;
+		case "Management":
+			display.innerHTML = "Corporate finance<br>Economics<br>Project management<br>Innovation management<br>Entrepreneurship<br>Accounting";
+			break;
+		case "Others":
+			display.innerHTML = "ARM7, FPGA Programming and Verilog<br>AI, Digital signal processing<br>Mathematics, Fundamental Algorithms and Control Engineering<br>Computer Assembly";
+			break;
+		case "Languages":
+			display.innerHTML = "English<br>French<br>Spanish";
+			break;
+		default:
+			display.innerHTML = "????????????";
+	}
+}
+function restoreSkillBox(skillboxID) {
+	var display = document.getElementById(skillboxID);
+	display.innerHTML = "";
+	display.innerHTML = skillboxID;
+}
 
 
 
@@ -92,12 +131,22 @@ function tag_cloud() {
 	  document.getElementById('myCanvasContainer').style.display = 'none';
 	}
 }
+function particles() {
+	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+	/* particlesJS.load('particles-js', '../config/particles.json', function() { */
+	particlesJS.load('particles', 'https://doc-14-3c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/76ta9eh2pgks3ccsoduqhel7suk8mc2f/1506988800000/00228922220177749400/*/0B5UPjZHikV5gS3p3TTNpc0hTRFE?e=download', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+}
+
+
 
 
 window.onload = function(){
-    smooth_scrolling();
     interactive_gradient_background();
     banner_opacity();
     center_download();
     tag_cloud();
+	smooth_scrolling();
+	particles();
 };
