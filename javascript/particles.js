a69116563ad12b6ba22e680208b4d586bca34593
Quentin McGaw <quentin.mcgaw@gmail.com>
2017-10-09 20:30:18 -0400

    Particles now work on mobile devices

--- javascript/particles.js
+++ javascript/particles.js
@@ -1,15 +1,18 @@
 var isMobile = false;
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){isMobile = true;}
-if(!isMobile){
-	console.log('particles.js: Desktop detected.');
-	$.getScript('javascript/particles.min.js',
-				function() {
-					/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-					/* particlesJS.load('particles-js', '../config/particles.json', function() { */
-					console.log('particles.js: particles.min.js loaded.');
-					particlesJS.load('particles', 'config/particles.json', function() {
-						console.log('particles.js: Particles launched.');
-					});
+
+$.getScript('javascript/particles.min.js',
+			function() {
+				/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+				/* particlesJS.load('particles-js', '../config/particles.json', function() { */
+				console.log('particles.js: particles.min.js loaded.');
+				var config = 'config/particles.json';
+				if(isMobile){
+					console.log('particles.js: Mobile detected.');
+					config = 'config/particles_mobile.json';
 				}
-				);
-}
+				particlesJS.load('particles', config, function() {
+					console.log('particles.js: Particles launched.');
+				});
+			}
+			);
