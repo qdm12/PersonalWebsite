c89a8fdea06c77e7dfe515ccbb22c3071c3c2310
Quentin McGaw <quentin.mcgaw@gmail.com>
2018-03-11 14:50:41 -0400

    Updated Others and using media queries

--- javascript/startup.js
+++ javascript/startup.js
@@ -36,11 +36,68 @@ function bannerOpacity() {
     });
 }
 
-window.onload = function () { /* executes when fully loaded */
-    $('#load-screen').fadeOut(function () {
-        $(this).remove();
-    });
-};
+$("#Skills > .skillbox").mouseover(function () {
+    switch (this.id) {
+    case "Crypto":
+        this.innerHTML = "Bitcoin, Ethereum, Cardano<br>Network and web security<br>Cryptography<br>Homomorphic encryption thesis<br>Kali Linux";
+        break;
+    case "Backend":
+        this.innerHTML = "Python, Golang, NodeJS<br>.NET, Java, Scala<br>RESTful, GraphQL<br>C++, C<br>GPUs with openCL";
+        break;
+    case "Frontend":
+        this.innerHTML = "HTML-5, CSS, JS<br>React Native, Android SDK<br>Cordova, PhoneGap<br>.NET, Kivy (Python)";
+        break;
+    case "Devops-and-Cloud":
+        this.innerHTML = "Docker, Vagrant and VirtualBox<br>Git, Github, Gitea and Gitlab<br>Zenhub, GitKraken, Swagger<br>Travis CI and unit testing<br>Google Cloud, AWS, Bluemix";
+        break;
+    case "Database-and-analytics":
+        this.innerHTML = "MySQL, SQlite<br>Redis<br>Hadoop and Map Reduce (Java, Python)<br>Impala, Hive, Spark";
+        break;
+    case "Design":
+        this.innerHTML = "Adobe XD, inVision<br>Gimp, Photofiltre<br>Patience and a decent taste";
+        break;
+    case "Server-admin":
+        this.innerHTML = "SSH, Shell, Bash scripting<br>Users and groups management<br>Apache, Nginx, Caddy<br>Ubuntu, Debian, CentOS, DSM";
+        break;
+    case "Management":
+        this.innerHTML = "Corporate finance<br>Economics<br>Entrepreneurship and startups<br>Project management<br>Innovation management<br>Accounting";
+        break;
+    case "Others":
+        this.innerHTML = "English, French, Spanish<br>ARM7, FPGA & Verilog<br>AI, Signal processing<br>Power engineering, Control Engineering<br>Computer architecture<br>Android OS, NFC";
+        break;
+    default:
+        this.innerHTML = "????????????";
+    }
+});
+$("#Skills > .skillbox").mouseleave(function () {
+    $(this).text(this.id.replace(new RegExp('-', 'g'), ' '));
+});
+
+$.getScript(
+    'javascript/particles.min.js',
+    function () {
+        if (isMobile) {
+            particlesJS.load('particles', 'config/particles_mobile.json');
+        } else {
+            particlesJS.load('particles', 'config/particles.json');
+        }
+    }
+);
+
+if (isMobile) {
+    $("#Projects > #github-feed").hide();
+} else {
+    $.getScript(
+        'javascript/github-activity.js',
+        function () {
+            GitHubActivity.feed({
+                username: "qdm12",
+                selector: "#github-feed"
+                // repository: "hbc"
+            });
+        }
+    );
+}
 
 $(document).ready(function () { /* executes first */
     var spinnerSize = Math.round(0.1 * Math.min($(window).height(),$(window).width()));
@@ -57,7 +114,6 @@ $(document).ready(function () { /* executes first */
     smooth_scrolling();
     bannerOpacity();
     if (!isMobile) {
-        // Banner opacity
         $("#Home").mousemove(function(event) {
             var pct = 70 * (event.pageX / $(this).width() + event.pageY / $(this).height());
             $("#Home").css("background-image", "linear-gradient(" + pct + "deg,#2f8770, #355d9f, #ff884d)");
@@ -68,7 +124,6 @@ $(document).ready(function () { /* executes first */
                 "font-size": "3vmax"
             }, 800);
         });
-        // Center download styling
         $("#center_download").mouseout(function () {
             $(this).stop().animate({
                 "opacity": "0.5",
@@ -76,6 +131,15 @@ $(document).ready(function () { /* executes first */
             }, 1200);
         });
     }
+    $("#Skills > .skillbox").each(function() {
+        $(this).text(this.id.replace(new RegExp('-', 'g'), ' '));
+    });
 });
+
+window.onload = function () { /* executes when fully loaded */
+    $('#load-screen').fadeOut(function () {
+        $(this).remove();
+    });
+};
 
 // $(window).resize(function() {});
